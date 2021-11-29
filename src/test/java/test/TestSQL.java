2f3094cbad93b19b360957dc68bcda6f220d5c1e
Elena Gorshenina <elenkagorshenina19.07.14@gmail.com>
2021-11-29 21:02:00 +0300

    Edited url 'open' in TestSQL

--- src/test/java/test/TestSQL.java
+++ src/test/java/test/TestSQL.java
@@ -10,7 +10,7 @@ public class TestSQL {
 
     @Test
     void successfulAuthTest() {
-        var loginPage = open("jdbc:mysql://185.119.57.47:3306/app", AuthPage.class);
+        var loginPage = open("http://185.119.57.9:9999", AuthPage.class);
         var authLogin = SQLUtils.getLogin();
         var authPassword = SQLUtils.getPassword();
         var verificationPage = loginPage.validLogin(authLogin, authPassword);
